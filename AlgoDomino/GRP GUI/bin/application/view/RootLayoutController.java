04ffc4da4df63539390cb4c05c2f87e792a642b8
WNVJungle <522569399@qq.com>
2017-02-27 22:55:13 +0800

    GUI
    GUI update

---
+++ AlgoDomino/GRP GUI/bin/application/view/RootLayoutController.java
@@ -0,0 +1,29 @@
+package unnc.cs.grape.view;
+
+/**
+ * 
+ */
+
+
+import java.io.IOException;
+import javafx.fxml.FXML;
+import application.MainApp;
+
+
+public class RootLayoutController {
+	private MainApp mainapp;
+	
+	@FXML
+	private void switchToMainFrame() throws IOException{
+		mainapp.showMainFrame();
+	}
+	
+	@FXML
+	private void showEfficiencyInterface() throws IOException{
+		mainapp.showEfficiencyInterface();
+	}
+	
+	
+	
+	
+}
